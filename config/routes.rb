bf700a306dbd7ec535d05a03e8d8dc078ac6c5cd
thaontn-0537 <nguyen.thi.ngoc.thao@sun-asterisk.com>
2024-07-24 08:21:42 +0700

    Revert "Chapter 3,4,5"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'demo_partials/new'
-  get 'demo_partials/edit'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # resources :static_pages, only: %i(home help) 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
